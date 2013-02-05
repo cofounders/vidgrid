a65a5b9ab3df7a3010d1afdaf2dda47b83c9f16b
Sebastiaan Deckers <sebdeckers83@gmail.com>
2013-02-05 18:49:19 +0800

    Create and query channel API

---
+++ server/server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var randword = require('randword');
+
+var channels = {};
+
+var allowCrossDomain = function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+}
+
+app.configure(function() {
+	app.use(allowCrossDomain);
+});
+
+app.get('/channel/:id', function(req, res) {
+	res.json(channels[req.params.id]);
+});
+
+app.post('/channel', function(req, res) {
+	randword(function(err, word) {
+		channels[word] = {hello: 'world', id: word};
+		res.json(channels[word]);
+	});
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
