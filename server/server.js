cb327e8baf3ab23ce593120f0d5884a39f5d2e71
Sebastiaan Deckers <sebdeckers83@gmail.com>
2013-02-10 15:49:09 +0800

    Implemented channel CRUD APIs

--- server/server.js
+++ server/server.js
@@ -13,20 +13,62 @@ var allowCrossDomain = function(req, res, next) {
 
 app.configure(function() {
 	app.use(allowCrossDomain);
-});
-
-app.get('/channel/:id', function(req, res) {
-	res.json(channels[req.params.id]);
+	app.use(express.bodyParser());
 });
 
 app.post('/channel', function(req, res) {
-	randword(function(err, word) {
-		channels[word] = {
-			id: word,
-			occupants: []
-		};
-		res.json(channels[word]);
-	});
+	var occupant = req.body.occupant;
+	if (occupant && occupant.id) {
+		randword(function(err, word) {
+			var channel = channels[word] = {
+				id: word,
+				occupants: [occupant]
+			};
+			res.json(channel);
+		});
+	} else {
+		res.json(403, {error: 'Invalid occupant'});
+	}
+});
+
+app.get('/channel/:id', function(req, res) {
+	var channel = channels[req.params.id];
+	if (channel)
+		res.json(channel);
+	else
+		res.json(404, {error: 'Channel not found'});
+});
+
+app.post('/channel/:id', function(req, res) {
+	var channel = channels[req.params.id],
+		occupant = req.body.occupant;
+	if (channel) {
+		channel.occupants.push(occupant);
+		res.json({status: 'success'});
+	} else {
+		res.json(404, {error: 'Channel not found'});
+	}
+});
+
+app.delete('/channel/:id', function(req, res) {
+	var channel = channels[req.params.id],
+		occupant = req.body.occupant,
+		isInChannel = function(channelOccupant) {
+			return occupant.id === channelOccupant.id;
+		},
+		isNotInChannel = function() { return !isInChannel(this, arguments); };
+
+	if (!channel)
+		return res.json(404, {error: 'Channel does not exist'});
+
+	if (!occupant || !occupant.id)
+		return res.json(403, {error: 'Invalid occupant'});
+
+	if (!channel.occupants.some(isInChannel))
+		return res.json({status: 'User was already gone'});
+
+	channel.occupants = channel.occupants.filter(isNotInChannel);
+	res.json({status: 'User kicked out'});
 });
 
 app.listen(3000);
